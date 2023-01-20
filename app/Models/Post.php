7f3349924c43fd336d1793d3db4a43da3031b0b7
TresorKL <trezmathieu@gmail.com>
2023-01-20 13:22:25 +0200

    set up Post model with jenssegers model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    protected $collection = 'posts_collection';
+    protected $primaryKey = 'id';
+    // MongoDB will also create _id, but the 'id' property will be used for primary key actions like find().
+    //Post::create(['id' => 1, 'title' => 'The Fault in Our Stars']);
+    protected $connection = 'mongodb';
 }
