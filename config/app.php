d03777bf1e16fbf5952608a12a192fce64a7aa23
TresorKL <trezmathieu@gmail.com>
2023-01-20 13:03:36 +0200

    add jenssegers mongodb service provider

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Jensseger\Mongodb\MongodbServiceProvider::class,
 
         /*
          * Package Service Providers...
